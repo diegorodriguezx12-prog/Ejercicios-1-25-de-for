0f882243c54f7926a4ec8f87217267336426781d
Diego Cifuentes <diego.rodriguezx12@outlook.es>
2026-02-23 22:22:08 -0600

    ejercicio 17

--- Program.cs
+++ Program.cs
@@ -1,4 +1,7 @@
-﻿for (int i = 1; i <= 10; i++)
+﻿int pares = 0;
+for (int i = 1; i <= 5; i++)
 {
-    Console.WriteLine(i);
+    Console.Write("Ingresa un número: ");
+    if (int.Parse(Console.ReadLine()) % 2 == 0) pares++;
 }
+Console.WriteLine("Total de números pares: " + pares);
