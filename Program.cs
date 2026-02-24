27a144cba6e6fb43eb4b335099c93d7b4b5b419c
Diego Cifuentes <diego.rodriguezx12@outlook.es>
2026-02-23 22:52:19 -0600

    ejercicio 24

--- Program.cs
+++ Program.cs
@@ -1,4 +1,17 @@
-﻿for (int i = 1; i <= 10; i++)
+﻿Console.Write("Cantidad de notas: ");
+int n = int.Parse(Console.ReadLine());
+double suma = 0;
+
+for (int i = 1; i <= n; i++)
 {
-    Console.WriteLine(i);
+    Console.Write($"Nota {i}: ");
+    suma += double.Parse(Console.ReadLine());
 }
+
+double promedioFinal = suma / n;
+Console.WriteLine($"Promedio: {promedioFinal}");
+
+if (promedioFinal >= 61)
+    Console.WriteLine("Estado: ¡APROBADO!");
+else
+    Console.WriteLine("Estado: REPROBADO.");
