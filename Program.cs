e83b273c3309a25193a6202bd1bd3c52ee75f1aa
Diego Cifuentes <diego.rodriguezx12@outlook.es>
2026-02-23 22:32:26 -0600

    ejercicio 20

--- Program.cs
+++ Program.cs
@@ -1,4 +1,10 @@
-﻿for (int i = 1; i <= 10; i++)
+﻿Console.Write("¿Cuántos números vas a ingresar?: ");
+int cantidad = int.Parse(Console.ReadLine());
+int positivos = 0;
+
+for (int i = 1; i <= cantidad; i++)
 {
-    Console.WriteLine(i);
+    Console.Write($"Número {i}: ");
+    if (int.Parse(Console.ReadLine()) > 0) positivos++;
 }
+Console.WriteLine($"Ingresaste {positivos} números positivos.");
