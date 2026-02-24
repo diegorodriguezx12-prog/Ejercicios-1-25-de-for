5520addf2357183f2f09ffb14fc8bc64cf57ee0a
Diego Cifuentes <diego.rodriguezx12@outlook.es>
2026-02-23 22:15:24 -0600

    ejercicio 15

--- Program.cs
+++ Program.cs
@@ -1,4 +1,7 @@
-﻿for (int i = 1; i <= 10; i++)
+﻿double suma = 0;
+for (int i = 1; i <= 5; i++)
 {
-    Console.WriteLine(i);
+    Console.Write($"Ingresa el número {i}: ");
+    suma += double.Parse(Console.ReadLine());
 }
+Console.WriteLine("El promedio es: " + (suma / 5));
