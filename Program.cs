30e039b337749680ae9fc6d86adf7d9ec32a5216
Diego Cifuentes <diego.rodriguezx12@outlook.es>
2026-02-23 22:37:30 -0600

    ejercicio 21

--- Program.cs
+++ Program.cs
@@ -1,4 +1,10 @@
-﻿for (int i = 1; i <= 10; i++)
+﻿Console.Write("¿Cuántos números ingresarás?: ");
+int n = int.Parse(Console.ReadLine());
+int negativos = 0;
+
+for (int i = 1; i <= n; i++)
 {
-    Console.WriteLine(i);
+    Console.Write($"Ingresa el número {i}: ");
+    if (int.Parse(Console.ReadLine()) < 0) negativos++;
 }
+Console.WriteLine($"Ingresaste un total de {negativos} números negativos.");
