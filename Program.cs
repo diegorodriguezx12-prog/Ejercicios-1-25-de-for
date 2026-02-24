946125392eb3908b7b74b411cc8aedfec3ed1723
Diego Cifuentes <diego.rodriguezx12@outlook.es>
2026-02-23 22:07:50 -0600

    ejercicio 13

--- Program.cs
+++ Program.cs
@@ -1,4 +1,6 @@
-﻿for (int i = 1; i <= 10; i++)
+﻿Console.Write("¿Desde qué número quieres bajar?: ");
+int n = int.Parse(Console.ReadLine());
+for (int i = n; i >= 1; i--)
 {
-    Console.WriteLine(i);
+    Console.Write(i + " ");
 }
