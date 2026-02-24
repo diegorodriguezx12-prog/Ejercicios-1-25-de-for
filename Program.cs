a09c7786648bab833231e866a3f9aca760e30742
Diego Cifuentes <diego.rodriguezx12@outlook.es>
2026-02-23 22:42:52 -0600

    ejercicio 22

--- Program.cs
+++ Program.cs
@@ -1,4 +1,10 @@
-﻿for (int i = 1; i <= 10; i++)
+﻿Console.Write("¿Cuántas ventas realizaste?: ");
+int n = int.Parse(Console.ReadLine());
+double totalVentas = 0;
+
+for (int i = 1; i <= n; i++)
 {
-    Console.WriteLine(i);
+    Console.Write($"Monto de la venta {i}: ");
+    totalVentas += double.Parse(Console.ReadLine());
 }
+Console.WriteLine($"El gran total de ventas es: {totalVentas:C}");
