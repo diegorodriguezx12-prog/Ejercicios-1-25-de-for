0e3f98f3367bf56e4265ba530e46bf67202615b1
Diego Cifuentes <diego.rodriguezx12@outlook.es>
2026-02-23 22:29:34 -0600

    ejercicio 19

--- Program.cs
+++ Program.cs
@@ -1,4 +1,8 @@
-﻿for (int i = 1; i <= 10; i++)
+﻿int menor = int.MaxValue;
+for (int i = 1; i <= 5; i++)
 {
-    Console.WriteLine(i);
+    Console.Write("Ingresa un número: ");
+    int num = int.Parse(Console.ReadLine());
+    if (num < menor) menor = num;
 }
+Console.WriteLine("El número más pequeño es: " + menor);
